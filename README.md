f9ebc41da80f9c91bfc5b180f2cf672011c63708
vanessadean <vanessa.dean@gmail.com>
2014-09-05 15:27:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+---
+tags: methods, return values, kids, ruby
+language: ruby
+level: 1
+type: lab
+---
+
+# A Subway Story
+
+Follow the instructions in subway_methods.rb and write the appropriate methods.
---
+++ methods_practice.rb
@@ -0,0 +1,22 @@
+# A subway story
+
+# You hop on the subway at Union Square. It takes about 156 seconds to go between stops and you'll be taking the train for 3 stops. Write a method that calculates how long your trip will take and puts it. 
+
+# While you are waiting for the train you decide to play a little dice. Create a method roll that puts a random number between 1 and 6 (feel free to google "how to generate a random number in ruby").
+
+# The train arrives and you hop on. Guess what time it is? It's showtime! There are 23 people on the train and each person gives the dancers 1.5 dollars. Write a method to yell at them in all caps to stop dancing.
+
+# There is a really enthusiastic rider who keeps shouting "Everything is awesome!" Write a method that puts everything is awesome 5 times. (This may require a little ruby research.)
+
+# You are almost at your stop and you start thinking about how you are going to get home. You have $18 dollars left on your metro card. Write a method that puts how many trips you have left.
+
+# Call your methods below:
+puts "The trip will take this long"
+# call your method here
+puts "While I was waiting I rolled a"
+# call your method here
+puts "The train dancers should really"
+# call your method here
+puts "That guy kept shouting"
+# call your method here
+puts "This is how any trips I have left"
