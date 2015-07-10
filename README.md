a739dc56e4a3704bf082cf5ea60e9b3c2cdda87e
Dammy Akinfenwa <dammy.akinfenwa@gmail.com>
2015-07-10 15:50:05 +0000

    adds spec folder and files needed for tests. did not change README.md instructions

---
+++ README.md
@@ -0,0 +1,14 @@
+---
+tags: methods, return values, kids, ruby
+language: ruby
+level: 1
+type: lab
+---
+
+# A Subway Story
+
+![subway balloons](http://1.media.collegehumor.cvcdn.com/28/26/beeb4cb32e3896b1659854ad8977c847-the-new-york-city-subway-what-you-can-expect.jpg)
+
+Fork and clone this lab and follow the instructions in `subway_methods.rb` to write the appropriate methods. 
+
+The first method is written out for you as an example. Don't forget to run your program after you are done.
---
+++ subway_methods.rb
@@ -0,0 +1,53 @@
+# A subway story
+
+# You hop on the subway at Union Square. As you are waiting for the train you take a look at the subway map. The map is about 21 inches wide and 35 inches tall. Let's write a method to puts the area of the map:
+def map_size
+  puts "The map is #{21 * 25} square inches"
+end
+
+# Now you give it a shot! It takes about 156 seconds to go between stops and you'll be taking the train for 3 stops. Write a method that puts how long your trip will take. 
+def trip_length
+	# write your code here
+end
+
+# While you are waiting for the train you decide to play a little dice. Create a method called roll that puts a random number between 1 and 6 (feel free to google "how to generate a random number in ruby").
+
+# write your code here
+
+
+
+# The train arrives and you hop on. Guess what time it is? It's showtime! There are 23 people on the train and each person gives the dancers 1.5 dollars. Write a method called pay_dancers that puts how much money they made. 
+
+
+# There is one grumpy lady on the train that doesn't like the dancing though. Write a method called stop_dancing that puts a message to the dancers in all caps.
+
+
+# There is also a really enthusiastic rider who keeps shouting "Everything is awesome!" Write a method called everything_is_awesome that puts everything is awesome 5 times. (Ruby has a times method that can help you do this.)
+
+
+# You are almost at your stop and you start thinking about how you are going to get home. You have $18 dollars left on your metro card. Write a method called remaining_trips that puts how many trips you have left.
+
+
+
+# Call your methods below:
+puts "How big is that subway map?"
+# call your method here - like this
+map_size
+
+puts "This is how long the trip will take"
+trip_length
+
+puts "While I was waiting I rolled a"
+# call your method here
+
+puts "How much money did the train dancers make?"
+# call your method here
+
+puts "That lady told the train dancers to"
+# call your method here
+
+puts "That guy kept shouting"
+# call your method here
+
+puts "This is how many trips I have left on my metrocard"
+# call your method here
